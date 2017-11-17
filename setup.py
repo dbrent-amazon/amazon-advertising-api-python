e14cf83af1dc4c47b399b7ed588548b0a25fcc51
Sarah <sarah@efficientera.com>
2017-11-17 11:02:21 -0800

    EEB-1310 - Fix install issue

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ import amazon_advertising_api.versions as aa_versions
 
 
 setup(
-    name='amazon_advertising',
-    packages=['amazon_advertising'],
+    name='amazon_advertising_api',
+    packages=['amazon_advertising_api'],
     version=aa_versions.versions['application_version'],
     description='Unofficial Amazon Sponsored Products Python client library.',
     url='https://github.com/sguermond/amazon-advertising-api-python')
