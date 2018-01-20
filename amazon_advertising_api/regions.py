b248308befea29cf39ed0ce5978de3b221a4c554
Sarah <sarah@efficientera.com>
2018-01-19 16:49:27 -0800

    Add FE endpoint to regions

--- amazon_advertising_api/regions.py
+++ amazon_advertising_api/regions.py
@@ -3,4 +3,7 @@ regions = {'na': {'sandbox': 'advertising-api-test.amazon.com',
                   'token_url': 'api.amazon.com/auth/o2/token'},
            'eu': {'sandbox': 'advertising-api-test.amazon.com',
                   'prod': 'advertising-api-eu.amazon.com',
+                  'token_url': 'api.amazon.com/auth/o2/token'},
+           'fe': {'sandbox': 'advertising-api-test.amazon.com',
+                  'prod': 'advertising-api-fe.amazon.com',
                   'token_url': 'api.amazon.com/auth/o2/token'}}
